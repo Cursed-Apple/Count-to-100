38ad22afb6bc5f9e960914d45c4072a8c5db1003
Cursed_Apple <144391684+Cursed-Apple@users.noreply.github.com>
2025-01-06 07:54:39 +0000

    the actualy upgrade

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ let count = 1
 function Increment()
 {
     increase++;
+    
+    if(increase > 50)
+        {
+            increase++;
+        }
+    if(increase > 200)
+        {
+            increase++;
+        }
+    if(increase > 500)
+        {
+            increase++;
+        }
+    
     countDisplay.innerText = increase;
-    if(increase > (50 * count))
-    {
-        count++;
-        increase++;
-    }
 }
