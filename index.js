969d193914bcb692bae089899a003e64499e0d79
Cursed_Apple <144391684+Cursed-Apple@users.noreply.github.com>
2025-01-06 07:48:19 +0000

    upgrading the click button

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-
 let countDisplay = document.getElementById("increasing");
 let increase = 0
+let count = 1
 function Increment()
 {
     increase++;
     countDisplay.innerText = increase;
-    if(increase >= 50)
+    if(increase > (50 * count))
     {
+        count++;
         increase++;
     }
 }
